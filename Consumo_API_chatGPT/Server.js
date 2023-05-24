b98ccc9d69eced9ed7f65518f30cc98b67e50df9
Ulisses Antonelli <112758646+Ulisses-Antonelli@users.noreply.github.com>
2023-05-24 10:19:45 -0300

    Add files via upload

---
+++ Consumo_API_chatGPT/Server.js
@@ -0,0 +1,35 @@
+//importação das dependencias
+const { geraJson } = require("./App"); 
+const express = require('express');
+const handlebars = require("express-handlebars").engine
+const app = express();
+const path = require('path') 
+const bodyParser = require('body-parser');
+
+
+
+app.engine("handlebars", handlebars({defaultLayout: "main"}))
+app.set("view engine", "handlebars")
+
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get('/', async (req, res) => {
+    try {
+      const data = await geraJson();
+      //console.log(data);
+      //res.json(data);
+      res.render('index', data); 
+    } catch (error) {
+      console.error("Erro ao gerar o JSON:", error);
+      res.status(500).json({ error: "Erro ao gerar o JSON" });
+    }
+  });
+  
+
+app.listen(8080, function(){
+    console.log("O servidor está ativo")
+});
---
+++ Consumo_API_chatGPT/public/index_3.js
@@ -0,0 +1,50 @@
+const botao01 = { id: 'botao_01', value: 'data.correta' };
+const botao02 = { id: 'botao_02', value: 'errada1' };
+const botao03 = { id: 'botao_03', value: '{{errada2}}' };
+const botao04 = { id: 'botao_04', value: '{{errada3}}' };
+
+const conjuntoTags = [botao01, botao02, botao03, botao04];
+shuffleArray(conjuntoTags);
+
+const grupoBotoes = document.getElementById('grupobotoes');
+grupoBotoes.innerHTML = ''; // Limpa o conteúdo anterior
+
+conjuntoTags.forEach(tag => {
+  const botaoElement = document.createElement('div');
+  botaoElement.className = 'botao col mb-3';
+  botaoElement.innerHTML = `<button id="${tag.id}" class="btn btn-primary btn-lg answer-button w-100 my-custom-text">${tag.value}</button>`;
+  grupoBotoes.appendChild(botaoElement);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var responseClone;
+  fetch('http://localhost:8080/')
+    .then(function(response) {
+      responseClone = response.clone();
+      return response.json();
+    })
+    .then(function(data) {
+      const botao01Element = document.getElementById('botao_01');
+      const botao02Element = document.getElementById('botao_02');
+      const botao03Element = document.getElementById('botao_03');
+      const botao04Element = document.getElementById('botao_04');
+
+      botao01Element.innerText = data.correta;
+      botao02Element.innerText = data.errada1;
+      botao03Element.innerText = data.errada2;
+      botao04Element.innerText = data.errada3;
+    },function(reject){ 
+      console.log('Erro + 1', reject, 'responseClone' + responseClone);
+      responseClone.text()
+      .then(function (btx){
+        console.log('Texto: ???', btx);
+      })
+    })
+});
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
